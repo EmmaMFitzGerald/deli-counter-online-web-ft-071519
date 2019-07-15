d292dc51bbeb955533c982ea6aeb12c6d7a7af37
Emma FitzGerald <emmamariafitzgerald@gmail.com>
2019-07-15 18:29:15 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -21,7 +21,7 @@ def now_serving(katz_deli)
   if katz_deli.length == 0 
     puts "There is nobody waiting to be served!"
   elsif katz_deli.length > 0 
-    katz_deli.shift
+    puts 
   end
 end
   
