ff3dd40512b8e5a207bc0d164209949723866c42
Emma FitzGerald <emmamariafitzgerald@gmail.com>
2019-07-15 18:11:33 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,7 +3,7 @@ katz_deli = []
 def line(katz_deli)
   if katz_deli.length > 0
     katz_deli.each_with_index do |name, index|
-    puts "Welcome, #{name}. You are number #{index + 1} in line."
+    puts "The line is currently #{index + 1}. #{name}."
     end
   elsif katz_deli.length == 0
     puts "The line is currently empty."
