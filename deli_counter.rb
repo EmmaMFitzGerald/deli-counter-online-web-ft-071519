b0f0d4be313ba1388582735b398c55e493bc9fec
Emma FitzGerald <emmamariafitzgerald@gmail.com>
2019-07-15 18:13:49 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,12 +1,13 @@
 katz_deli = []
 
 def line(katz_deli)
-  phrase = "The line is currently "
+  phrase = "The line is currently: "
   if katz_deli.length > 0
     katz_deli.each_with_index do |name, index|
     phrase += "#{index + 1}. #{name}."
     end
-  elsif katz_deli.length == 0
+    puts phrase
+  else
     puts "The line is currently empty."
   end
 end
