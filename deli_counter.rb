04dd1f7ef9dacd2e1e6cef4810fc844aad342085
Emma FitzGerald <emmamariafitzgerald@gmail.com>
2019-07-15 18:08:53 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,10 @@
-# Write your code here.
+katz_deli = []
+
+def line(katz_deli)
+  if katz_deli.length > 0
+  katz_deli.each_with_index do |name, index|
+    puts "Welcome, #{name}. You are number #{index + 1} in line."
+  else
+    "The line is currently empty"
+  end
+end
